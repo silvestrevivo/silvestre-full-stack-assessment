76b6935cd8e5acf01c046e432979b71bfb238ac1
silvestrevivo <silvestrevivomillan@gmail.com>
2024-09-17 19:19:24 +0200

    url proxy adjusted

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://silvestre-full-stack-assessment.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
